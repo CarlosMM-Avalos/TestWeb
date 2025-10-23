dbdd77fcaf16d770c3b7ad45aa47d641248cb22c
CarlosMM-Avalos <carlosmmavalos@gmail.com>
2025-10-23 00:19:34 -0300

    primer commit

---
+++ code.js
@@ -0,0 +1,6 @@
+const titulo = document.getElementById("titulo1");
+const boton = document.getElementById("boton1");
+
+boton.addEventListener("click", function(){
+    titulo.style.color = "blue";
+})
